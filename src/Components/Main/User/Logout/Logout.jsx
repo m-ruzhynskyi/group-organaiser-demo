ba6ce56740d585d61c56da4d9fa2b2c680e91f83
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2024-03-16 20:12:03 +0200

    start category (more) small add

--- src/Components/Main/User/Logout/Logout.jsx
+++ src/Components/Main/User/Logout/Logout.jsx
@@ -1,19 +1,19 @@
 import './logout.css'
 import {useDispatch} from "react-redux";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {logoutUser} from "../../../../store/userSlice";
+import {useEffect} from "react";
 export default function Logout() {
     const dispatch = useDispatch()
-    // const user = useSelector(state => state.user.user)
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
     function handleLogout() {
         dispatch(logoutUser())
         localStorage.removeItem('user')
     }
 
-    // useEffect(() => {
-    //     if(user === null) navigate('/')
-    // }, []);
+    useEffect(() => {
+        if(!localStorage.user) navigate('/')
+    }, []);
     return(
         <div className={'logoutPage'}>
             <button className={'logout__logout'} onClick={handleLogout}><Link to={'/#'}>Logout</Link></button>
--- src/Components/Main/User/More/Category/Category.jsx
+++ src/Components/Main/User/More/Category/Category.jsx
@@ -2,9 +2,6 @@ import modules from "./category.module.css";
 import CreateBlock from "./CreateBlock";
 import {useEffect} from "react";
 import {Route, Routes, useNavigate} from "react-router-dom";
-import Moderate from "../Options/Moderate/Moderate";
-import Journal from "../Options/Journal/Journal";
-import GroupMiss from "../Options/GroupMiss/GroupMiss";
 
 export default function Category({user}) {
     const navigate = useNavigate()
@@ -16,19 +13,22 @@ export default function Category({user}) {
     }
 
     return (
-        <Routes>
-            <Route path={''} element={
-                <div className={modules.variantsBlock}>
-                <div className={modules.variants} style={{width: (user === 'helper' || user === 'admin') ? '100%' : ''}}>
-                    <CreateBlock nextPage={nextPage}/>
-                    {(user === 'helper' || user === 'admin') && <CreateBlock type={'first'} nextPage={nextPage} user={user}/>}
-                    {(user === 'admin') && <CreateBlock type={'second'} nextPage={nextPage} user={user}/>}
-                </div>
-            </div>
-            }/>
-            <Route path={'moderate/*'} element={<Moderate/>}/>
-            <Route path={'journal/*'} element={<Journal/>}/>
-            <Route path={'group/*'} element={<GroupMiss/>}/>
-        </Routes>
+        <>
+
+        </>
+        // <Routes>
+        //     <Route path={''} element={
+        //         <div className={modules.variantsBlock}>
+        //         <div className={modules.variants} style={{width: (user === 'helper' || user === 'admin') ? '100%' : ''}}>
+        //             <CreateBlock nextPage={nextPage}/>
+        //             {(user === 'helper' || user === 'admin') && <CreateBlock type={'first'} nextPage={nextPage} user={user}/>}
+        //             {(user === 'admin') && <CreateBlock type={'second'} nextPage={nextPage} user={user}/>}
+        //         </div>
+        //     </div>
+        //     }/>
+        //     <Route path={'moderate/*'} element={<Moderate/>}/>
+        //     <Route path={'journal/*'} element={<Journal/>}/>
+        //     <Route path={'group/*'} element={<GroupMiss/>}/>
+        // </Routes>
     )
 }
