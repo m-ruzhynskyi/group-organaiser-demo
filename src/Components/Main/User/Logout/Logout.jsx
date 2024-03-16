eb8b672259aca8857877b0d07343fa758223467b
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2024-03-16 16:39:10 +0200

    complete logout section

--- src/Components/Main/User/Logout/Logout.jsx
+++ src/Components/Main/User/Logout/Logout.jsx
@@ -1,16 +1,22 @@
 import './logout.css'
 import {useDispatch} from "react-redux";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {logoutUser} from "../../../../store/userSlice";
 export default function Logout() {
     const dispatch = useDispatch()
+    // const user = useSelector(state => state.user.user)
+    // const navigate = useNavigate()
     function handleLogout() {
         dispatch(logoutUser())
         localStorage.removeItem('user')
     }
+
+    // useEffect(() => {
+    //     if(user === null) navigate('/')
+    // }, []);
     return(
-        <div className={'logout'}>
-            <button className={'logout__logout'} onClick={handleLogout}><Link to={'/'}>Logout</Link></button>
+        <div className={'logoutPage'}>
+            <button className={'logout__logout'} onClick={handleLogout}><Link to={'/#'}>Logout</Link></button>
             <button className={'logout__cancel'}><Link to={'/more'}>Cancel</Link></button>
         </div>
     )
