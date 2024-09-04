3f56cba4fa16965ca0668b0dcca0a621c4215428
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2024-09-04 17:20:19 +0300

    add login hints

--- src/Components/Main/User/Login/Login.jsx
+++ src/Components/Main/User/Login/Login.jsx
@@ -5,34 +5,40 @@ import {checkUserInput} from "../../../../store/userSlice";
 import './login.css'
 import {useNavigate} from "react-router-dom";
 
-export default function Login(){
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const user = useSelector(state => state.user.user)
+export default function Login() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const user = useSelector(state => state.user.user)
 
-    function textEnter(e){setLogin(e.target.value)}
-    function passwordEnter(e){setPassword(e.target.value)}
-    function handleVerify(e){
-        e.preventDefault();
-        e.stopPropagation();
-        dispatch(checkUserInput({login, password}))
-        setPassword('')
+  function textEnter(e) {
+    setLogin(e.target.value)
+  }
+
+  function passwordEnter(e) {
+    setPassword(e.target.value)
+  }
+
+  function handleVerify(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    dispatch(checkUserInput({login, password}))
+    setPassword('')
+  }
+
+
+  useEffect(() => {
+    if (user !== null) {
+      navigate('/more')
+      localStorage.setItem('user', user)
     }
-
-
-    useEffect(() => {
-        if(user !== null){
-            navigate('/more')
-            localStorage.setItem('user',user)
-        }
-        // eslint-disable-next-line
-    }, [user]);
-    return(
-            <div className={'login'}>
-                <LoginPage passwordEnter={passwordEnter} textEnter={textEnter} password={password} text={login}
-                           handle={handleVerify}/>
-            </div>
-    )
+    // eslint-disable-next-line
+  }, [user]);
+  return (
+    <div className={'login'}>
+      <LoginPage passwordEnter={passwordEnter} textEnter={textEnter} password={password} text={login}
+                 handle={handleVerify}/>
+    </div>
+  )
 }
--- src/Components/Main/User/Login/LoginPage/LoginPage.jsx
+++ src/Components/Main/User/Login/LoginPage/LoginPage.jsx
@@ -2,23 +2,34 @@ import "./loginPage.css";
 import {useSelector} from "react-redux";
 import Loader from "../../../../Reusable/Loader/Loader";
 
-export default function LoginPage({passwordEnter, textEnter, text, password, handle}){
-    const status = useSelector(state => state.user.status)
+export default function LoginPage({passwordEnter, textEnter, text, password, handle}) {
+  const status = useSelector(state => state.user.status)
 
-    return(
-            <div className={'loginForm'}>
-                {!status ? (
-                    <>
-                        <h1 className={'loginForm__text'}>Login</h1>
-                        <form onSubmit={handle} className={'loginForm__form'}>
-                            <input type="text" value={text} placeholder='Name...' required
-                                   onChange={textEnter}/>
-                            <input type="password" value={password} placeholder='Password...' required
-                                   onChange={passwordEnter}/>
-                            <button className={'loginForm__button'}>Login</button>
-                        </form>
-                    </>): <Loader/>}
-
+  return (
+    <div className={'loginForm'}>
+      {!status ? (
+        <>
+          <h1 className={'loginForm__text'}>Login</h1>
+          <form onSubmit={handle} className={'loginForm__form'}>
+            <input type="text" value={text} placeholder='Name...' required
+                   onChange={textEnter}/>
+            <input type="password" value={password} placeholder='Password...' required
+                   onChange={passwordEnter}/>
+            <button className={'loginForm__button'}>Login</button>
+          </form>
+          <div className={'hint__bar'}>
+            <div className={'hint'}>
+              <p className={'hint__text'}>Name: admin; password: admin</p>
             </div>
-    )
+            <div className={'hint'}>
+              <p className={'hint__text'}>Name: helper; password: helper</p>
+            </div>
+            <div className={'hint'}>
+              <p className={'hint__text'}>Name: writer; password: writer</p>
+            </div>
+          </div>
+        </>) : <Loader/>}
+
+    </div>
+  )
 }
