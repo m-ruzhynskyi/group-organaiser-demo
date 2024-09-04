d465d1dac506f329104612ef1d70f983212561ec
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2024-09-04 17:09:05 +0300

    fix journal

--- src/Components/Main/User/More/JournalMore/JournalMore.jsx
+++ src/Components/Main/User/More/JournalMore/JournalMore.jsx
@@ -7,44 +7,44 @@ import {setBlackout, setMissingInfo} from "../../../../../store/blackoutSlice";
 
 
 export default function JournalMore() {
-    const [date, setDate] = useState([])
-    const [completedDate, setCompletedDate] = useState('')
-    const [monthData, setMonthData] = useState([])
-    const {groupName, schedulesList} = useSelector(state => state.schedules)
-    const journal = useSelector(state => state.journal.journal)
-    const dispatch = useDispatch()
+  const [date, setDate] = useState([])
+  const [completedDate, setCompletedDate] = useState('')
+  const [monthData, setMonthData] = useState([])
+  const {groupName, schedulesList} = useSelector(state => state.schedules)
+  const journal = useSelector(state => state.journal.journal)
+  const dispatch = useDispatch()
 
-    function setDateNext(e) {
-        const newCompletedDate = e.target.textContent
-        setCompletedDate(newCompletedDate)
+  function setDateNext(e) {
+    const newCompletedDate = e.target.textContent
+    setCompletedDate(newCompletedDate)
 
-        let setMonth = Number(newCompletedDate.split('.')[1]) - 1
-        const filteredData = journal[setMonth].filter(element => element['Дата'] === newCompletedDate);
-        setDate(filteredData)
+    let setMonth = Number(newCompletedDate.split('.')[1]) - 1
+    const filteredData = journal[setMonth].filter(element => element['Дата'] === newCompletedDate);
+    setDate(filteredData)
 
-        dispatch(setBlackout())
+    dispatch(setBlackout())
+  }
+
+  useEffect(() => {
+    setMonthData(sortDates(journal))
+    // eslint-disable-next-line
+  }, [journal]);
+
+  useEffect(() => {
+    if (date.length > 0) {
+      const missingStudents = getMissingStudents(date, schedulesList[0][groupName]['students'], schedulesList[0][groupName]['subjects']);
+      dispatch(setMissingInfo({
+        date: completedDate,
+        mode: 'journal',
+        missInfo: missingStudents
+      }));
     }
+    // eslint-disable-next-line
+  }, [date]);
 
-    useEffect(() => {
-        setMonthData(sortDates(journal))
-        // eslint-disable-next-line
-    }, [journal]);
-
-    useEffect(() => {
-        if (date.length > 0) {
-            const missingStudents = getMissingStudents(date, schedulesList[0][groupName]['students'], schedulesList[0][groupName]['subjects']);
-            dispatch(setMissingInfo({
-                date: completedDate,
-                mode: 'journal',
-                missInfo: missingStudents
-            }));
-        }
-        // eslint-disable-next-line
-    }, [date]);
-
-    return (
-        <div className={'journalMore'}>
-            <CreateMonths monthData={monthData} setData={setDateNext}/>
-        </div>
-    )
+  return (
+    <div className={'journalMore'}>
+      <CreateMonths monthData={monthData} setData={setDateNext}/>
+    </div>
+  )
 }
--- src/Components/functions/sortDates.js
+++ src/Components/functions/sortDates.js
@@ -1,33 +1,34 @@
 export default function sortDates(data) {
-    function quickSort(arr) {
-            if (arr.length <= 1) return arr;
-            const pivot = arr[0];
-            const left = [];
-            const right = [];
-            for (let i = 1; i < arr.length; i++) {
-                if (arr[i] < pivot) left.push(arr[i]);
-                else right.push(arr[i]);
-            }
-            return [...quickSort(left), pivot, ...quickSort(right)];
-        }
+  function quickSort(arr) {
+    if (arr.length <= 1) return arr;
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) left.push(arr[i]);
+      else right.push(arr[i]);
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  }
 
-        let tempArray = []
-        data.forEach(months => {
-            months.forEach(element => {
-                let date = element['Дата']
-                let month = element['Дата'].split('.')[1]
-                if (!tempArray[month]) tempArray[month] = []
-                tempArray[month].push(date);
-            })
-        })
-        let uniqArray = []
-        for (let i = 0; i < Object.values(tempArray).length; i++) {
-            let month = Object.values(tempArray)[i]
-            month = month.map(day => Number(day.split('.')[0]))
-            if (month.length >= 1) {
-                uniqArray.push(Array.from(new Set(month)));
-            }
-        }
-        let sortedArr = uniqArray.map(element => quickSort(element))
-        return sortedArr.map((element, id) => element.map(day => `${day.toString().padStart(2, '0')}.${(id + 1).toString().padStart(2, '0')}.2024`))
+  let tempArray = []
+  data.forEach(months => {
+    months.forEach(element => {
+      let date = element['Дата']
+      let month = element['Дата'].split('.')[1]
+      if (!tempArray[month]) tempArray[month] = []
+      tempArray[month].push(date);
+    })
+  })
+  let uniqArray = []
+  const months = Object.keys(tempArray)
+  for (let i = 0; i < Object.values(tempArray).length; i++) {
+    let month = Object.values(tempArray)[i]
+    month = month.map(day => Number(day.split('.')[0]))
+    if (month.length >= 1) {
+      uniqArray.push(Array.from(new Set(month)));
+    }
+  }
+  let sortedArr = uniqArray.map(element => quickSort(element))
+  return sortedArr.map((element, id) => element.map(day => `${day.toString().padStart(2, '0')}.${(Number(months[id])).toString().padStart(2, '0')}.${new Date().getFullYear()}`))
 }
